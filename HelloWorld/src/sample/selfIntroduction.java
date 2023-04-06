61bded3cab7b3ce08300fbdd83960e0e743155cb
“Haruna <ex436000@gmail.com>
2023-04-06 23:52:12 +0900

    課題1-3新規作成

---
+++ HelloWorld/src/sample/selfIntroduction.java
@@ -0,0 +1,21 @@
+package sample;
+
+public class selfIntroduction {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		//氏名
+		String name = "田中 晴菜";
+		//年齢
+		int age = 27;
+		//趣味
+		String hobby = "旅行に行くこと";
+		/*
+		*自己紹介文を出力する処理
+		 */
+		System.out.println("私の名前は" + name + "です。");
+		System.out.println("年齢は" + age + "歳です。");
+		System.out.println("趣味は" + hobby + "です。");
+	}
+
+}
