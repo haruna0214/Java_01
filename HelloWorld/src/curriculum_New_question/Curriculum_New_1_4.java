f480d305fe7adcab60c550f9f1653db3b781d6d7
“Haruna <ex436000@gmail.com>
2023-04-12 22:01:09 +0900

    Q3までの記述を保存

--- HelloWorld/src/curriculum_New_question/Curriculum_New_1_4.java
+++ HelloWorld/src/curriculum_New_question/Curriculum_New_1_4.java
@@ -1,29 +1,68 @@
 package curriculum_New_question;
 
 public class Curriculum_New_1_4 {
-
+	// Q1 バイト型
+	public static byte a;
+	// Q1 短整数型
+	public static short b;
+	// Q1 整数型
+	public static int c;
+	// Q1 長整数型
+	public static long d;
+	// Q1 単精度浮動小数点数型
+	public static float e;
+	// Q1 倍精度浮動小数点数型
+	public static double f;
+	// Q1 文字型
+	public static char g;
+	// Q1 文字列型
+	public static String h;
+	// Q1 ブーリアン型 
+	public static boolean i;
+	
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
 		 // Q1 下記9個をクラス変数として宣言のみしてください
-	     //		・バイト型・短整数型・整数型・長整数型
-	     // 	・単精度浮動小数点数型・倍精度浮動小数点数型
-	     //   	・文字型・文字列型
-	     //	 	・ブーリアン型 
-   	 
+		
 	     // Q2 それぞれのクラス変数をローカル内でそれぞれの初期値を代入し初期化してください
-   	 
+		// バイト型
+		a = 0;
+		// 短整数型
+		b = 0;
+		// Q1 整数型
+		c = 0;
+		// Q1 長整数型
+		d = 0;
+		// 単精度浮動小数点数型
+		e = 0.0F;
+		// Q1 倍精度浮動小数点数型
+		f = 0.0;
+		// Q1 文字型
+		g = '\u0000';
+		// Q1 文字列型
+		h = null;
+		// Q1 ブーリアン型 
+		i = false;
 	     // Q3 初期化をしたそれぞれの変数に下記の値を代入してください
 	
 	     // ・バイト型                 10
+		a = 10;
 	     // ・短整数型                 100
+		b = 100;
 	     // ・整数型                	 1000
+		c = 1000;
 	     // ・長整数型                 10000
+		d = 10000;
 	     // ・単精度浮動小数点数型   	 9.5
+		e = 9.5F;
 	     // ・倍精度浮動小数点数型		 10.5
+		f = 10.5;
 	     // ・文字型                   a
+		g = 'a';
 	     // ・文字列型              	 ハロー
+		h = "ハロー";
 	     // ・ブーリアン型          	 true
-	    	 
+	    i = true;
 	     // Q4 下記の通りにコンソール出力されるようにしてください
 	     // 必ず変数を使用すること
 	
