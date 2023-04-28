0e044bb335e0f1f32da07253d02c50bb7a119827
“Haruna <ex436000@gmail.com>
2023-04-28 19:23:14 +0900

    回答済み

---
+++ HelloWorld/src/practice_for_1_10/Practice.java
@@ -0,0 +1,26 @@
+/*
+問題.for文を使用し、以下のプログラムを作成してください。
+	①変数iに1を代入
+	②変数iに2ずつ足す処理を10回繰り返す。
+		出力結果：3
+				5
+				7
+				9
+				・
+				・
+				・
+				21
+*/
+package practice_for_1_10;
+
+public class Practice {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		// *変数iに1を代入する*
+		for (int i = 3; i < 22; i +=2){
+			System.out.println(i);
+		}
+	}
+
+}
